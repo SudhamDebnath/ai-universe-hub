b14f261e1949cdb333d08645ad05b80d37fc9d07
SudhamDebnath <sudham15-12854@diu.edu.bd>
2023-03-03 20:38:12 +0600

    Featchin data from url

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('AI Universe Hub')
+const loadAllData = () => {
+    fetch("https://openapi.programming-hero.com/api/ai/tools")
+        .then((res) => res.json())
+        .then((data) => {
+            // console.log(data.data.tools[0].name);
+            console.log(data.data);
+            showAllData(data.data);
+        });
+
+};
+
+const showAllData = (universeAi) => {
+ universeAi.forEach((universe) => {
+    console.log(universe)
+ });
+  
+};
+
+loadAllData();
